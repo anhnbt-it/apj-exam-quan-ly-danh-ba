85303eed73465fa5a1ed19560a12d70cf113dd1a
anhnbt-it <anhnbt.it@gmail.com>
2020-11-10 09:10:50 +0700

    feat: add DanhBa.java

--- src/DanhBa.java
+++ src/DanhBa.java
@@ -6,4 +6,60 @@
  */
 
 public class DanhBa {
+    private String hoTen;
+    private String soDienThoai;
+    private String diaChi;
+    private String email;
+    private String facebook;
+
+    public DanhBa() {
+    }
+
+    public DanhBa(String hoTen, String soDienThoai, String diaChi, String email, String facebook) {
+        this.hoTen = hoTen;
+        this.soDienThoai = soDienThoai;
+        this.diaChi = diaChi;
+        this.email = email;
+        this.facebook = facebook;
+    }
+
+    public String getHoTen() {
+        return hoTen;
+    }
+
+    public void setHoTen(String hoTen) {
+        this.hoTen = hoTen;
+    }
+
+    public String getSoDienThoai() {
+        return soDienThoai;
+    }
+
+    public void setSoDienThoai(String soDienThoai) {
+        this.soDienThoai = soDienThoai;
+    }
+
+    public String getDiaChi() {
+        return diaChi;
+    }
+
+    public void setDiaChi(String diaChi) {
+        this.diaChi = diaChi;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFacebook() {
+        return facebook;
+    }
+
+    public void setFacebook(String facebook) {
+        this.facebook = facebook;
+    }
 }
