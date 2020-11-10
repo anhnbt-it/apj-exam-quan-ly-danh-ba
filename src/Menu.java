cfe4a4e51533aa94e1723ed8458dc3a404431c98
anhnbt-it <anhnbt.it@gmail.com>
2020-11-10 09:15:40 +0700

    feat: add Menu.java

---
+++ src/Menu.java
@@ -0,0 +1,22 @@
+/**
+ * Created by IntelliJ IDEA.
+ * User: AnhNBT (anhnbt.it@gmail.com)
+ * Date: 10/11/2020
+ * Time: 9:12 SA
+ */
+
+public class Menu {
+    void show() {
+        System.out.println("---- CHƯƠNG TRÌNH QUẢN LÝ DANH BẠ ----");
+        System.out.println("Chọn chức năng theo số (để tiếp tục): ");
+        System.out.println("1. Xem danh sách");
+        System.out.println("2. Thêm mới");
+        System.out.println("3. Cập nhật");
+        System.out.println("4. Xóa");
+        System.out.println("5. Tìm kiếm");
+        System.out.println("6. Đọc từ file");
+        System.out.println("6. Ghi vào file");
+        System.out.println("8. Thoát");
+        System.out.print("Chọn chức năng: ");
+    }
+}
